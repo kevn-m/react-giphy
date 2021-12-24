804d623ffab14e7da73fc52578ea7383ad0b8f55
Kevin Mok <kevin@phoria.com.au>
2021-12-24 11:18:15 +1100

    added selected gif section

---
+++ src/components/selected_gif.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react"
+
+class SelectedGif extends Component {
+  render() {
+    return <div className="selected-gif"></div>
+  }
+}
+
+export default SelectedGif
