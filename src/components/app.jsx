ce33d753c919f779519778a5c3a5a2bb6258277a
Kevin Mok <kevin@phoria.com.au>
2021-12-24 11:18:01 +1100

    add search bar component with styling

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,7 +1,18 @@
 import React from "react"
 
+import SearchBar from "./search_bar"
+import SelectedGif from "./selected_gif"
+
 const App = () => {
-  return <div>Hello world</div>
+  return (
+    <div>
+      <div className="left-scene">
+        <SearchBar />
+        <SelectedGif />
+      </div>
+      <div className="right-scene"></div>
+    </div>
+  )
 }
 
 export default App
---
+++ src/components/search_bar.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react"
+
+class searchBar extends Component {
+  render() {
+    return (
+      <div className="form-search">
+        <input type="text" />
+      </div>
+    )
+  }
+}
+
+export default searchBar
